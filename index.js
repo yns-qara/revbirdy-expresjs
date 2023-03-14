ea7b705005d1cb71b33e85197626840f13a6510b
yns-qara <younesqara@gmail.com>
2023-03-14 17:01:33 +0100

    social media

--- index.js
+++ index.js
@@ -19,6 +19,30 @@ connection.connect((error) => {
 });
 
 
+
+// function handleDisconnect() {
+//     connection = mysql.createConnection(db_config); // Recreate the connection, since
+//     // the old one cannot be reused.
+
+//     connection.connect(function (err) {              // The server is either down
+//         if (err) {                                     // or restarting (takes a while sometimes).
+//             console.log('error when connecting to db:', err);
+//             setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+//         }                                     // to avoid a hot loop, and to allow our node script to
+//     });                                     // process asynchronous requests in the meantime.
+//     // If you're also serving http, display a 503 error.
+//     connection.on('error', function (err) {
+//         console.log('db error', err);
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//             handleDisconnect();                         // lost due to either server restart, or a
+//         } else {                                      // connnection idle timeout (the wait_timeout
+//             throw err;                                  // server variable configures this)
+//         }
+//     });
+// }
+
+// handleDisconnect();
+
 const app = express();
 app.use(express.static('public'))
     .use(express.urlencoded({ extended: false }));
@@ -50,6 +74,6 @@ app.post('/submit', (req, res) => {
 
 
 
-app.listen(8080, () => {
-    console.log('Server started on port 8080');
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
