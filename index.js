c56a260c7dc56933f308c284b2c760a62c8a6575
yns-qara <younesqara@gmail.com>
2023-03-14 13:44:44 +0100

    switchin the main file 😗

--- index.js
+++ index.js
@@ -1,70 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const { response } = require('express');
+const express = require('express');
+require('dotenv').config();
 
-// Set up express app
-const app = express();
 
-// Create connection to MySQL database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'younesqara',
-  database: 'revdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-// Connect to MySQL database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
-
-// Set up middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-    response.send("done");
-});
-
-// Set up route to handle form submissions
-app.post('/submit-email', (req, res) => {
-  const email = req.body.email;
-
-  // Validate email format
-  if (!isValidEmail(email)) {
-    return res.status(400).send('Invalid email format');
-  }
-
-  // Verify email using external service or manual verification
-  // ...
-
-  // Save email to database
-  const sql = 'INSERT INTO emails (email) VALUES (?)';
-  connection.query(sql, [email], (err, result) => {
-    if (err) {
-      if (err.code === 'ER_DUP_ENTRY') {
-        return res.status(409).send('Email already exists');
-      } else {
-        console.error('Error saving email to MySQL database:', err);
-        return res.status(500).send('Internal server error');
-      }
+connection.connect((error) => {
+    if (error) {
+        console.log('Error connecting to database:', error);
+    } else {
+        console.log('Connected to database');
     }
-    res.status(200).send('Email saved successfully');
-  });
 });
 
-// Start server
+
+const app = express();
+app.use(express.static('public'))
+    .use(express.urlencoded({ extended: false }));
+
+app.post('/submit', (req, res) => {
+    const email = req.body.email;
+
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        console.log('Invalid email format');
+        res.send('Invalid email format');
+        return;
+    }
+
+    const query = 'INSERT INTO users (email) VALUES (?)';
+    const values = [email];
+
+    connection.query(query, values, (error, results) => {
+        if (error) {
+            console.log('Error inserting email:', error);
+            res.send('Error inserting email');
+        } else {
+            console.log('Email inserted:', results);
+            res.send('Email submitted');
+        }
+    });
+});
+
+
+
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
-
-// Helper function to validate email format
-function isValidEmail(email) {
-  // Use regex to validate email format
-  const regex = '/^[^\s@]+@[^\s@]+\.[^\s@]+$/';
-  return regex.test(email);
-}
